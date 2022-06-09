12fb42d9ad330e10a5539eeea019cdf4a2095a7f
Michael David Galibut <dave@loudcloud.ph>
2022-06-09 17:34:22 +0800

    created the form to create new employee

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,26 @@
 class EmployeesController < ApplicationController
+
+  def new_employee
+    @employee = Employee.new
+  end
+
+  def show
+  end
+
+
+  def create
+    @employee = Employee.new(employee_params)
+    if @employee.save
+      # Handle a successful save.
+      redirect_to dashboard_url, notice: "Employee account created"
+    else
+      render 'new'
+    end
+  end    
+
+  private
+
+    def employee_params
+      params.require(:employee).permit(:email, :name, :admin, :password, :password_confirmation)
+    end    
 end
